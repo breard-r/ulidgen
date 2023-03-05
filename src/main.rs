e6da4a58621a2f24999a6e4dfb8b3a1a0a1c015d
Rodolphe Bréard <rodolphe@what.tf>
2023-03-05 13:51:09 +0100

    Allow to generate multiple ulid

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,18 @@ use uuid::Uuid;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+	#[arg(short, long, default_value_t = 1, help = "Number of ulid to generate")]
+	nb: usize,
 	#[arg(short, long, help = "Display the ulid using the uuid format")]
 	uuid: bool,
 }
 
 fn main() {
 	let args = Args::parse();
-	let ulid = generate(&args);
-	display(&args, ulid);
+	for _ in 0..args.nb {
+		let ulid = generate(&args);
+		display(&args, ulid);
+	}
 }
 
 #[inline]
